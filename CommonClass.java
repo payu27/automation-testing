827528aa6d2ffc122182c4b7cb0e6b5751dfb7c7
payu27 <100690208+payu27@users.noreply.github.com>
2022-12-16 13:57:51 +0530

    Add files via upload

---
+++ CommonClass.java
@@ -0,0 +1,29 @@
+package com.cls;
+
+public class CommonClass 
+{
+public static void main(String[] args)
+{
+	String s1 = new String("  This is tops Technoloes..");
+	System.out.println("Original String is.."+s1);
+	System.out.println("length is.."+s1.length());
+	
+	System.out.println("Upper case is .."+s1.toUpperCase());
+	System.out.println("Lower case is.."+s1.toLowerCase());
+	
+	System.out.println("Substring is .."+s1.substring(2));
+	System.out.println("Substring is.."+s1.substring(2, 4));
+	
+	System.out.println("Char at is.."+s1.charAt(2));
+	System.out.println("trim is.."+s1.trim());
+	
+	if(s1.equalsIgnoreCase("  This is tops Technoloes..")) 
+	{
+		System.out.println("Both are match");
+	}
+	else
+	{
+		System.out.println("Both are not match");
+	}
+}
+}
